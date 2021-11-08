50fb40e04c840fe3a44d5707e716bc0b4dcfe9e3
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-08 08:53:24 +0100

    Initial commit

---
+++ matrix_vector.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv){
+    int processes, rank;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&processes);
+
+    MPI_Finalize();
+    return 0;
+}
