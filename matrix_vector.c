6a00ff72e24135ec27f9b0d8a264d7deee533402
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-09 11:40:30 +0100

    removed unused macros

--- matrix_vector.c
+++ matrix_vector.c
@@ -2,16 +2,11 @@
 #include <mpi.h>
 #include <stdlib.h>
 
-#define MIN(a, b)           ((a)<(b)?(a):(b))
 #define BLOCK_LOW(id, p, n)  ((id)*(n)/(p))
 #define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id)+1,p,n)-1)
 #define BLOCK_SIZE(id, p, n) \
                      (BLOCK_HIGH(id,p,n)-BLOCK_LOW(id,p,n)+1)
-#define BLOCK_OWNER(j, p, n) (((p)*((j)+1)-1)/(n))
-#define PTR_SIZE           (sizeof(void*))
-#define CEILING(i, j)       (((i)+(j)-1)/(j))
 
-#define MAX_ITERATIONS 1
 
 int main(int argc, char** argv){
     int processes, rank;
