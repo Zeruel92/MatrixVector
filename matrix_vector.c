bc8b7abf2afc28474ade372c8b63463aedb95045
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-09 11:39:52 +0100

    Parallel multiply

--- matrix_vector.c
+++ matrix_vector.c
@@ -21,6 +21,7 @@ int main(int argc, char** argv){
     int **matrix=NULL;
     int *matrix_storage=NULL;
     int *vector=NULL;
+    int *temp_vector=NULL;
     int *output_vector=NULL;
 
     MPI_Request request;
@@ -67,7 +68,8 @@ int main(int argc, char** argv){
     }
 
     vector = (int *) malloc(n* sizeof(int));
-
+    temp_vector = (int *) calloc(size,sizeof(int));
+    if(!rank) output_vector = (int *) malloc(m*sizeof(int));
     //Loading array
     if(rank == processes-1){
         for(int i = 0; i < processes-1; i++) {
@@ -115,9 +117,10 @@ int main(int argc, char** argv){
     for (int iterations = 0; iterations < MAX_ITERATIONS; iterations++) {
         for(int i =0;i<size;i++){
             for(int j=0; j<n; j++){
-
+                temp_vector[i]+= matrix[i][j] * vector[j];
             }
         }
+        MPI_Gather(temp_vector,size,MPI_INT,output_vector,m,MPI_INT,0,MPI_COMM_WORLD);
     }
 
 
@@ -127,25 +130,10 @@ int main(int argc, char** argv){
     MPI_Finalize();
 
 #ifdef _DEBUG
-    if (!rank) {
-        fprintf(stdout, "OUTPUT MATRIX:\n");
-        fflush(stdout);
-    } else {
-        MPI_Recv(&debug, 1, MPI_INT, (rank - 1) % processes, 1, MPI_COMM_WORLD, &status);
-    }
-    for (int i = 0; i < size; i++) {
-        for (int k = 0; k < n; k++) {
-            fprintf(stdout, "%d ", matrix[i][k]);
-            fflush(stdout);
-        }
-        fprintf(stdout, "\n");
-        fflush(stdout);
-    }
-    if (processes > 1) MPI_Send(&debug, 1, MPI_INT, (rank + 1) % processes, 1, MPI_COMM_WORLD);
     if(!rank){
         fprintf(stdout,"VECTOR:\n");
-        for(int i = 0; i <n; i++)
-            fprintf(stdout,"%d ",vector[i]);
+        for(int i = 0; i <m; i++)
+            fprintf(stdout,"%d ",output_vector[i]);
         fprintf(stdout,"\n");
     }
 #endif
@@ -153,6 +141,8 @@ int main(int argc, char** argv){
     free(matrix),matrix=NULL;
     free(matrix_storage),matrix_storage=NULL;
     free(vector),vector=NULL;
+    free(temp_vector), temp_vector=NULL;
+    if(!rank)free(output_vector), output_vector=NULL;
     if(rank==processes-1) fclose(matrix_file);
 
     if(!rank) fprintf(stdout,"Processes %d Iterations: %d Time: %f",processes,MAX_ITERATIONS,elapsed);
